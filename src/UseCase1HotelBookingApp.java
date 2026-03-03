d75cd6e881327620b50e55bf2089154be93292ba
Vidush1224 <vidush.skv@gmail.com>
2026-03-03 09:43:50 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ src/UseCase1HotelBookingApp.java
@@ -0,0 +1,6 @@
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args){
+        System.out.println("Welome to Hotel Booking Management System");
+        System.out.println("System initialized successfully");
+    }
+}
